e9de0e6b2eb30988aa63c17c0f3eeb8e3651b609
Brandon Eng <bce25@cornell.edu>
2020-08-19 20:50:33 -0700

    styling and multiple requests

--- src/BarChart.jsx
+++ src/BarChart.jsx
@@ -1,52 +1,60 @@
-import React, { Component } from 'react';
-import * as d3 from 'd3'; //https://www.freecodecamp.org/news/how-to-get-started-with-d3-and-react-c7da74a5bd9f/#:~:text=You%20need%20to%20use%20import,has%20no%20default%20exported%20module.
+import React, { Component } from "react";
+import * as d3 from "d3"; //https://www.freecodecamp.org/news/how-to-get-started-with-d3-and-react-c7da74a5bd9f/#:~:text=You%20need%20to%20use%20import,has%20no%20default%20exported%20module.
 
 const BarChart = (props) => {
-    
-    const { committers } = props;
+  const { committers } = props;
 
-    const cData = Array.from(committers);
+  const cData = Array.from(committers);
 
-    // const testData = [ 2, 1, 5, 3, 4 ];
+  // const testData = [ 2, 1, 5, 3, 4 ];
 
-    const drawBarChart = (data) => {
-        const canvasHeight = 400;
-        const canvasWidth = 600;
-        const scale = 5;
+  const drawBarChart = (data) => {
+    const canvasHeight = 400;
+    const canvasWidth = 600;
+    const scale = 5;
 
-        const svgCanvas = d3.select("#canvas")
-            .append("svg")
-            .attr("width", canvasWidth)
-            .attr("height", canvasHeight)
-            .style("border", "1px solid black");
-        
-        svgCanvas.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("width", 40)
-            .attr("height", (datapoint) => datapoint[1] * scale)
-            .attr("fill", "orange")
-            .attr("x", (datapoint, iteration) => iteration * 45)
-            .attr("y", (datapoint) => canvasHeight - datapoint[1] * scale);
-        
-        svgCanvas.selectAll("text")
-            .data(data)
-            .enter()
-            .append("text")
-            .attr("x", (dataPoint, i) => i * 45 + 15)
-            .attr("y", (dataPoint, i) => canvasHeight - dataPoint[1] * scale - 10)
-            .text(dataPoint => dataPoint[1])
-    }
+    const svgCanvas = d3
+      .select("#canvas")
+      .append("svg")
+      .attr("width", canvasWidth)
+      .attr("height", canvasHeight)
+      .style("border", "1px solid black");
 
-    return (
-        <>
-            <div id="canvas"></div> 
-            {
-                // drawBarChart(testData)
-                drawBarChart(cData)
-            }
-        </>
-    )
-}
-export default BarChart
+    svgCanvas
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("width", 40)
+      .attr("height", (datapoint) => datapoint[1] * scale)
+      .attr("fill", "orange")
+      .attr("x", (datapoint, iteration) => iteration * 45)
+      .attr("y", (datapoint) => canvasHeight - datapoint[1] * scale);
+
+    svgCanvas
+      .selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .attr("x", (dataPoint, i) => i * 45 + 15)
+      .attr("y", (dataPoint, i) => canvasHeight - dataPoint[1] * scale - 10)
+      .text((dataPoint) => dataPoint[1]);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div id="canvas"></div>
+      {
+        // drawBarChart(testData)
+        drawBarChart(cData)
+      }
+    </div>
+  );
+};
+export default BarChart;
--- src/GithubAxios.jsx
+++ src/GithubAxios.jsx
@@ -1,108 +1,126 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import BarChart from './BarChart';
-
-const getRandomColor = () => {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-const getNColors = (n) => {
-    let colors = [];
-    for (var i = 0; i < n; i ++){
-        colors.append(getRandomColor());
-    }
-    return colors;
-}
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import BarChart from "./BarChart";
 
 const GithubAxios = () => {
+  const [commits, setCommits] = useState([]);
 
-    const [commits, setCommits] = useState([]);
+  // can use these variable to change owner and repository
+  const owner = "bitcoin";
+  const repo = "bitcoin";
 
-    useEffect(() => {
-
-        const owner = "bitcoin";
-        const repo = "bitcoin";
-
-        // https://developer.github.com/v3/repos/commits/#list-commits
-        axios.get(`https://api.github.com/repos/${owner}/${repo}/commits`)
-        .then(res => {
-            console.log(res);
-            const data = res.data;
-            setCommits(data);
+  async function asyncFunc() {
+    // can use this variable to fetch more commits
+    const numer_of_pages = 1;
+    let page = 1;
+    let total = [];
+    while (page < numer_of_pages + 1) {
+      const url = `https://api.github.com/repos/${owner}/${repo}/commits?page=${page}`;
+      console.log(url);
+      await axios
+        .get(url)
+        .then((res) => {
+          console.log("data", res.data);
+          total = total.concat(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
         });
-    }, []);
-
-    const committers = new Map();
-    for (var c of commits){
-        let author = c.commit.author.name;
-        if (committers.has(author)){
-            committers.set(author, committers.get(author)+1);
-        }
-        else {
-            committers.set(author, 1);
-        }
+      page++;
     }
+    setCommits(total);
+  }
 
-    const keys = Array.from(committers.keys())
+  useEffect(() => {
+    asyncFunc();
+  }, []);
 
-    const data = {
-        name: "all_data",
-        children: []
+  // counts the number of commits per author
+  const committers = new Map();
+  for (var c of commits) {
+    let author = c.commit.author.name;
+    if (committers.has(author)) {
+      committers.set(author, committers.get(author) + 1);
+    } else {
+      committers.set(author, 1);
+    }
+  }
+
+  // formats data for D3 bar graph
+  const keys = Array.from(committers.keys());
+  const data = {
+    name: "all_data",
+    children: [],
+  };
+  for (var k of keys) {
+    let entry = {
+      name: k,
+      value: committers.get(k),
     };
+    data.children.push(entry);
+  }
 
-    for (var k of keys){
-        let entry = {
-            name: k,
-            value: committers.get(k)
-        };
-        data.children.push(entry);
-    }
-
-    return (
-        <div>
-            <table>
-                <tbody>
-                    {   
-                        // https://stackoverflow.com/questions/43885365/using-map-on-an-iterator
-                        keys.map((k) => {
-                            return (
-                                <tr key={k}>
-                                    <td>{k}</td>
-                                    <td>{committers.get(k)}</td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-            <hr/>
-            <BarChart committers={committers}/>
-            <hr/>
-            <table>
-                <tbody>
-                    {
-                        commits.map(c => {
-                            let author = c.commit.author.name;
-                            let date = c.commit.author.date;
-                            let message = c.commit.message;
-                            return (
-                                <tr key={date}>
-                                    <td>{author}</td>
-                                    <td>{date}</td>
-                                    <td>{message}</td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <h2 style={{ textAlign: "center" }}>Owner: {owner}</h2>
+      <h2 style={{ textAlign: "center" }}>Repository: {repo}</h2>
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Developer</th>
+            <th scope="col">Commits</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            // https://stackoverflow.com/questions/43885365/using-map-on-an-iterator
+            keys.map((k) => {
+              return (
+                <tr key={k}>
+                  <td>{k}</td>
+                  <td>{committers.get(k)}</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+      <hr />
+      <h3 style={{ textAlign: "center" }}>
+        Bar Chart showing commits per person
+      </h3>
+      <BarChart committers={committers} />
+      <hr />
+      <h3 style={{ textAlign: "center" }}>Commits</h3>
+      <table
+        className="table table-dark"
+        style={{ tableLayout: "fixed", width: "100%" }}
+      >
+        <thead>
+          <tr>
+            <th scope="col">Developer</th>
+            <th scope="col">Date</th>
+            <th scope="col">Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {commits.map((c) => {
+            let author = c.commit.author.name;
+            let date = c.commit.author.date;
+            let message = c.commit.message;
+            return (
+              <tr key={date}>
+                <td>{author}</td>
+                <td>{date}</td>
+                <td>{message}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default GithubAxios;
