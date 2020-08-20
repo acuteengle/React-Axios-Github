9a1f7f135d46a4b31c31ba5999fe8cc600e2d876
Brandon Eng <bce25@cornell.edu>
2020-08-19 20:51:21 -0700

    Updates

---
+++ precache-manifest.da90a22de2b00e8573cb5fa882980291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d97fdead6ee5e5aab3cf12353750d7",
+    "url": "/ReactAxiosGithub/index.html"
+  },
+  {
+    "revision": "30511e67567a3424412c",
+    "url": "/ReactAxiosGithub/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cf2090eeda80754b1313",
+    "url": "/ReactAxiosGithub/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30511e67567a3424412c",
+    "url": "/ReactAxiosGithub/static/js/2.16f3bd44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAxiosGithub/static/js/2.16f3bd44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2090eeda80754b1313",
+    "url": "/ReactAxiosGithub/static/js/main.9e027f85.chunk.js"
+  },
+  {
+    "revision": "a0a48776d5f5219b3c7f",
+    "url": "/ReactAxiosGithub/static/js/runtime-main.b738c152.js"
+  }
+]);
